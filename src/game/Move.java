5e4ac2c60f117bc646c7782ab6da04e92909e16c
rezajebeli97 <rezajebeli97@yahoo.com>
2018-07-24 00:43:17 +0430

    base script

---
+++ src/game/Move.java
@@ -0,0 +1,25 @@
+package game;
+
+public class Move {
+
+    private int[][] board;
+    private BoardSquare boardPlace;
+
+    public Move(int[][] b, BoardSquare bp) {
+        board = b;
+        boardPlace = bp;
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public BoardSquare getBardPlace() {
+        return boardPlace;
+    }
+
+	public void setBoard(int[][] tab) {
+		this.board = tab;		
+	}
+
+}
